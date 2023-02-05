7beae31bcd909e8006738dcb7afacc852b565d9f
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-02-05 17:25:29 +0000

    add edit and debug

--- codestar/settings.py
+++ codestar/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 import os
 import dj_database_url
-from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -31,8 +30,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['django-blog-866.herokuapp.com', 'localhost']
 
@@ -61,14 +58,6 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-
-MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-info',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
-    }
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
